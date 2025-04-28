4ce9e240dabfbdf426d497aedd97ab80e7fc1042
Aki Fukuda <akif.3839@keio.jp>
2025-04-28 17:00:12 +0900

    chapter4-4-1

---
+++ ch4/ch4-4/pydoc_getall.py
@@ -0,0 +1,37 @@
+import os, requests, urllib, time
+import urllib.parse
+from bs4 import BeautifulSoup
+
+save_dir = './pydoc_tutorial'
+top_page = 'https://docs.python.org/ja/3/tutorial/index.html'
+check_url = 'https:/docs.python.org/ja/3/tutorial'
+
+visited = {}
+
+def get_page(url):
+    if check_url not in url:
+        return
+    if url in visited:
+        return
+
+    res = requests.get(url)
+    res.encoding = res.apparent_encoding
+    html = res.text
+    visited[url] = True
+
+    fname = save_dir + "/" + os.path.basename(url)
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+    with open(fname, 'wb') as f:
+        f.write(html.encode('utf-8'))  # ←ここも本当は後述の注意あり
+        print('save', fname)
+
+    time.sleep(1)
+    soup = BeautifulSoup(html, 'html5lib')
+    for a in soup.find_all('a'):
+        a_url = urllib.parse.urljoin(url, a.get('href', ''))
+        a_url = urllib.parse.urldefrag(a_url)[0]
+        get_page(a_url)
+
+if __name__ == '__main__' :
+    get_page(top_page)
